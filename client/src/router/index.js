659b640b56904e1c55086df0fbb5a51791575222
Aditya <aditya_201712026@outlook.com>
2018-06-07 17:58:41 +0530

    Directory structure

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import BookDescription from '../components/book-description'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/bookdescription',
+      name: 'BookDescription',
+      component: BookDescription
     }
   ]
 })
