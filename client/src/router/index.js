c02872cd9a1033b6c6ab2c6db97f78cfd1786cef
DivineInfinity <galaxytwirls@gmail.com>
2018-06-08 15:25:43 +0530

    removed unused files

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '../components/home'
+import reviews from '../components/reviews'
 Vue.use(Router)
 
 export default new Router({
@@ -9,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: home
+    },
+    {
+      path: '/reviews',
+      name: 'reviews',
+      component: reviews
     }
   ]
 })
