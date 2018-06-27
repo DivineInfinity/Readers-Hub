dba0e778132658005bb0f26d93d9cb30512a8fb8
Deepak Lalwani <deepak.lalwani81@gmail.com>
2018-06-28 03:37:07 +0530

    added logo & fixed bug with vue-disqus

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,7 +5,7 @@
 
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-      <b-navbar-brand href="#">Reader's Hub</b-navbar-brand>
+      <b-navbar-brand href="#"><img src="../src/assets/logo1.png" height="50" width="80"></b-navbar-brand>
 
       <b-collapse is-nav id="nav_collapse">
 
