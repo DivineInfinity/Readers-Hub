f45d9bd696d4c12b8d4017cd1b223b57334e3aca
Deepak Lalwani <deepak.lalwani81@gmail.com>
2018-07-06 18:23:36 +0530

    added user id to user object

--- server/models/review.js
+++ server/models/review.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const reviewSchema=mongoose.Schema({
     _id:mongoose.Schema.ObjectId,
-    user:{name:String,profilePic:String},
+    user:{userId:mongoose.Schema.ObjectId,name:String,profilePic:String},
     review:String,
     reviewTitle:String,
     rating:Date,
